e3fc50f1bd831d348a04fdbf3f540c04293ad550
Rodrigo Rael <rodrigorael53@gmail.com>
2024-04-03 12:40:22 -0300

    :sparkles: feat: products CRUD

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
 class Product(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +35,54 @@ def delete_product(product_id):
 
     return jsonify({ "message": 'Product deleted successfully' })
   return jsonify({ "message": 'Product not found' }), 404
+
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_product_details(product_id):
+  product = Product.query.get(product_id)
+
+  if product:
+    return jsonify({
+      "id": product.id,
+      "name": product.name,
+      "price": product.price,
+      "description": product.description,
+    })
+  return jsonify({ "message": 'Product not found' }), 404
+
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+  product = Product.query.get(product_id)
+
+  if not product:
+    return jsonify({ "message": 'Product not found' }), 404
+  
+  data = request.json
+  if 'name' in data:
+    product.name = data['name']
+
+  if 'price' in data:
+    product.price = data['price']
+
+  if 'description' in data:
+    product.description = data['description']
+
+  db.session.commit()
+  return jsonify({ "message": 'Product updated successfully' })
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+  products = Product.query.all()
+
+  product_list = []
+  for product in products:
+    product_data = {
+      "id": product.id,
+      "name": product.name,
+      "price": product.price,
+    }
+    product_list.append(product_data)
+
+  return jsonify(product_list)
 
 @app.route('/')
 def hello_world():
